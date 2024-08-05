3b9cd7d35f762342f0bfc4c2fe807fa1041eb395
atouba <anasstouba5@gmail.com>
2024-08-05 23:12:55 +0300

    fix quotes puntiuation
    doc: make tests and finalize project making sure everything is
    working

--- main.go
+++ main.go
@@ -28,23 +28,51 @@ func main() {
 	writeOutput(tokens, out_file)
 }
 
+// check if a token that start with a ' is an actual word (i.e len > 0)
+// and that it's not at the start of the line
+// so as to determine whether to add a space before the token (word) or not
+func isNonStarterWordWithAQuote(tokens []string, word_index int) bool {
+	// length of the word preceding the word in question
+	// needed to see if its last character is a newline
+	l_pre_word := len(tokens[word_index - 1])
+	if tokens[word_index][0] == '\'' && len(tokens[word_index]) > 1 &&
+	word_index > 0 && tokens[word_index - 1][l_pre_word - 1] != '\n' {
+		return true
+	}
+	return false
+}
+
 func writeOutput(tokens []string, out_file *os.File) {
-	//  a word is considered an actual word preceded by a space
-	//  and appended by a punctituation mark if there's any
+	//  a word is considered an actual word preceded by a space  and appended by a punctituation mark if there's any except in the case of ' where there is 2 cases
 	var word string
 	for i := 0; i < len(tokens); i++ {
-		if tokens[i][0] == ' ' {
-			continue
-		}
+		if tokens[i][0] == ' ' { continue }
 		word = tokens[i]
-		if !isPunctuation(rune(tokens[i][0])) && i != 0 {
-			word = piscine.Concat(" ", word)
+		if (!isPunctuation(rune(tokens[i][0])) ||
+		isNonStarterWordWithAQuote(tokens, i)) &&
+		i != 0 {
+			word = " " + word
 		}
-		if i < len(tokens) - 1 && isPunctuation(rune(tokens[i + 1][0])) {  // how come tokens[][0] is not rune
-			word = piscine.Concat(word, tokens[i + 1])
+		if i + 1 < len(tokens) && isPunctuation(rune(tokens[i + 1][0])) {  // how come tokens[][0] is not rune
+			if tokens[i + 1][0] == '\'' && quoteCounting(tokens, i) % 2 == 0 {
+				tokens[i + 2] = tokens[i + 1] + tokens[i + 2]
+			} else {
+				word = piscine.Concat(word, tokens[i + 1])
+			}
 			tokens[i + 1] = " "
 		}
 		out_file.WriteString(word)
 		// out_file.WriteString(" ")
 	}
+}
+
+func quoteCounting(tokens []string, index_word int) int {
+	count := 0
+
+	for i := index_word; i < len(tokens); i++ {
+		if tokens[i] == "'" {
+			count++
+		}
+	}
+	return count
 }
